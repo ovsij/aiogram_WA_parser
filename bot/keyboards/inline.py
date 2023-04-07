05e943da2916ddc041a5effe36fc8e38d6967aef
ovsij <v.ovsij@yandex.ru>
2023-04-08 00:30:12 +0300

    first commit

--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -332,7 +332,19 @@ def inline_kb_delcatalog():
     schema = [1]
     catalogs = get_catalogs()
     for catalog in catalogs:
-        text_and_data.append([emojize(f':x: {catalog.phone}', language='alias'), f'btn_delcatalog_{catalog.phone}'])
+        text_and_data.append([emojize(f':recycle: {catalog.phone}', language='alias'), f'btn_delcatalog_{catalog.phone}'])
+        schema.append(1)
+    text_and_data.append(btn_back('admin'))
+    inline_kb = InlineConstructor.create_kb(text_and_data, schema)
+    return text, inline_kb
+
+def inline_kb_editcatalog():
+    text = 'Выберите каталог, который хотите удалить'
+    text_and_data = []
+    schema = [1]
+    catalogs = get_catalogs()
+    for catalog in catalogs:
+        text_and_data.append([emojize(f':recycle: {catalog.phone}', language='alias'), f'btn_editcatalog_{catalog.phone}'])
         schema.append(1)
     text_and_data.append(btn_back('admin'))
     inline_kb = InlineConstructor.create_kb(text_and_data, schema)
