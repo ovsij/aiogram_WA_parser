21bc5e7d60493681c96bd1dbf875a4869bd1c811
ovsij <v.ovsij@yandex.ru>
2023-04-09 01:48:44 +0300

    first commit

--- bot/main.py
+++ bot/main.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     from handlers import dp
     loop = asyncio.get_event_loop()
     #loop.create_task(parser.get_valentino())
-    #loop.create_task(scheduled_catalogs(0))
+    loop.create_task(scheduled_catalogs(0))
     loop.create_task(send_mes(5))
     #loop.create_task(scheduled_valentino(7200))
     executor.start_polling(dp, skip_updates=True)
