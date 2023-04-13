c8b49bbe434b3818e2fe548d53736b16141d485e
ovsij <v.ovsij@yandex.ru>
2023-04-13 03:48:38 +0300

    first commit

--- bot/handlers/users/commands.py
+++ bot/handlers/users/commands.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from bot.keyboards.buttons import *
 from dotenv import load_dotenv
 import os
@@ -16,7 +17,19 @@ async def bot_start(message: types.Message):
     register_user(message.from_user)
     text, reply_markup = inline_kb_menu(message.from_user)
     #create_demo_orders(str(message.from_user.id))
-    if message.from_user.id in [227184505]:
+    if str(message.from_user.id) in os.getenv("ADMINS"):
+        reply_markup.add(btn_admin())
+    Form.menu_msg = await bot.send_message(
+        message.from_user.id,
+        text=text,
+        reply_markup=reply_markup
+    )
+
+@dp.message_handler(commands=['stop'], state=Form.add_catalog)
+async def bot_start(message: types.Message, state: FSMContext):
+    state.finish()
+    text, reply_markup = inline_kb_menu(message.from_user)
+    if str(message.from_user.id) in os.getenv('ADMINS'):
         reply_markup.add(btn_admin())
     Form.menu_msg = await bot.send_message(
         message.from_user.id,
